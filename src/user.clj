e2872283c44fa115a271ceede5979cb7342d66f3
Teodor Heggelund <git@teod.eu>
2023-08-18 11:38:32 +0200

    Add Clerk

--- src/user.clj
+++ src/user.clj
@@ -5,3 +5,11 @@
   (let [clerk-serve (requiring-resolve 'nextjournal.clerk/serve!)
         clerk-port 7053]
     (clerk-serve {:browse? true :port clerk-port})))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn clerk-start-watch! []
+  (let [clerk-serve (requiring-resolve 'nextjournal.clerk/serve!)
+        clerk-port 7053]
+    (clerk-serve {:browse? true
+                  :port clerk-port
+                  :watch-paths ["src"]})))
