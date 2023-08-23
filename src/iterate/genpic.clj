7c5ff512633f96240d11c084bf852b50cbe0357e
johanmartinejohnsen <jomarn@me.com>
2023-08-23 14:18:47 +0200

    delete example code

--- src/iterate/genpic.clj
+++ src/iterate/genpic.clj
@@ -2,16 +2,6 @@
   (:require [babashka.cli :as cli]
             [iterate.genpic.prompt2 :as prompt]))
 
-
-(defn branch-create [opts]
-  (println "branch create"))
-
-(defn branch-delete [opts]
-  (println "Branch delete!!!!"))
-
-(defn complete [opts]
-  (println "Complete!!!"))
-
 (defn ask [opts]
   (let [prompt (get-in opts [:opts :prompt])]
     (println (prompt/gpt-ask prompt (System/getenv "GENPIC_OPENAI_API_KEY")))
@@ -21,11 +11,10 @@
 (defn help [opts]
   (println "Nyttig hjelpetekst her ..."))
 
-(def subcommands [{:cmds ["branch" "create"] :fn branch-create}
-                  {:cmds ["branch" "delete"] :fn branch-delete}
-                  {:cmds ["complete"] :fn complete :args->opts [:prompt]}
+(def subcommands [
                   {:cmds ["ask"] :fn ask :args->opts [:prompt]}
                   {:cmds [] :fn help}])
 
 (defn -main [& args]
   (cli/dispatch subcommands args))
+ 
