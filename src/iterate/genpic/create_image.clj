33772e411392a381e9368836708dc964ef2c60b9
johanmartinejohnsen <jomarn@me.com>
2023-08-23 13:52:01 +0200

    exporting code from exploring namespace to new ns create-image

---
+++ src/iterate/genpic/create_image.clj
@@ -0,0 +1,21 @@
+(ns iterate.genpic.create-image
+  (:require
+   [babashka.http-client :as http]
+   [cheshire.core :as json]
+   [clojure.edn :as edn]
+   [nextjournal.clerk :as clerk]
+   [babashka.curl]))
+
+(defn openapi-post-generate
+  [opts api-key]
+  (def resp (http/post "https://api.openai.com/v1/images/generations"
+                       {:headers {:content-type "application/json"
+                                  :authorization (str "Bearer " api-key)}
+                        :body (json/generate-string {:prompt opts
+                                                     :n 2
+                                                     :size "1024x1024"})}))
+  (:data (json/parse-string (:body resp) true)))
+
+(defn gpt-generate [p api-key]
+  (->>
+   (openapi-post-generate p api-key)))
